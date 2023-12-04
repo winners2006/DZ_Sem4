6ac4f111c98de72e03ce2a80900e489cf150b8be
Winners2006-2 <winners2006-2@mail.ru>
2023-12-04 11:34:03 +0300

    res

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте массив заполненный случайными
+// трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в
+// массиве.
+
+Console.Write("Введите размер масива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+
+void AddMass(int[] arrNew){
+
+    Random ran = new Random();
+    for(int i = 0; i < arrNew.Length; i++){
+        arrNew[i] = ran.Next(100, 999);
+    }
+}
+
+void PrintMass(int[] arrPrint){
+    for(int i = 0; i<arrPrint.Length; i++){
+        Console.Write($"{arrPrint[i]} ");
+    }
+}
+
+void Result(int[] arrRes){
+    
+    int collect = 0;
+    foreach (int item in arrRes)
+    {
+        if(item % 2 ==0) collect++;
+    }
+    Console.WriteLine(collect);
+}
+
+
+AddMass(arr);
+PrintMass(arr);
+Console.WriteLine();
+Result(arr);
